4321935ec7f263d6e7f38c8217c02a502d7d19de
akradimo <fzeynabak@gmail.com>
2025-03-09 13:17:09 +0330

    8

--- person/person.php
+++ person/person.php
@@ -1,8 +1,6 @@
 <?php
 $page = '/hesabfa/person/person.php';
 include '../index.php';
-
-// اتصال به پایگاه داده
 include '../database.php';
 
 // دریافت لیست افراد
@@ -19,62 +17,95 @@ $result = executeQuery($sql);
         </div>
     <?php endif; ?>
 
-    <div class="bg-white shadow-md rounded my-6">
-        <table class="min-w-full table-auto">
-            <thead>
-                <tr class="bg-gray-200 text-gray-600 text-sm leading-normal">
-                    <th class="py-3 px-6 text-right">کد حسابداری</th>
-                    <th class="py-3 px-6 text-right">شرکت</th>
-                    <th class="py-3 px-6 text-right">عنوان</th>
-                    <th class="py-3 px-6 text-right">نام</th>
-                    <th class="py-3 px-6 text-right">نام خانوادگی</th>
-                    <th class="py-3 px-6 text-right">نام مستعار</th>
-                    <th class="py-3 px-6 text-right">دسته‌بندی</th>
-                    <th class="py-3 px-6 text-right">نوع</th>
-                    <th class="py-3 px-6 text-right">اعتبار مالی</th>
-                    <th class="py-3 px-6 text-right">تلفن</th>
-                    <th class="py-3 px-6 text-right">موبایل</th>
-                    <th class="py-3 px-6 text-right">آدرس</th>
-                    <th class="py-3 px-6 text-right">عملیات</th>
-                </tr>
-            </thead>
-            <tbody class="text-gray-600 text-sm">
-                <?php while ($row = $result->fetch_assoc()): ?>
-                    <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['code_hesabdari']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['company']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['title']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['name']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['family']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['nickname']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['category']); ?></td>
-                        <td class="py-3 px-6 text-right">
-                            <?php
-                            $types = [];
-                            if ($row['type_customer']) $types[] = 'مشتری';
-                            if ($row['type_supplier']) $types[] = 'تامین کننده';
-                            if ($row['type_shareholder']) $types[] = 'سهامدار';
-                            if ($row['type_employee']) $types[] = 'کارمند';
-                            echo htmlspecialchars(implode('، ', $types));
-                            ?>
-                        </td>
-                        <td class="py-3 px-6 text-right"><?php echo number_format($row['credit']); ?> ریال</td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['telephone']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['mobile']); ?></td>
-                        <td class="py-3 px-6 text-right"><?php echo htmlspecialchars($row['address_text']); ?></td>
-                        <td class="py-3 px-6 text-right">
-                            <div class="flex item-center justify-end">
-                                <a href="edit_person.php?id=<?php echo $row['id']; ?>" class="text-blue-600 hover:text-blue-900 ml-2">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <a href="delete_person.php?id=<?php echo $row['id']; ?>" class="text-red-600 hover:text-red-900 ml-2" onclick="return confirm('آیا از حذف این فرد اطمینان دارید؟')">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                            </div>
-                        </td>
+    <div class="bg-white shadow-md rounded my-6 overflow-x-auto">
+        <div class="min-w-max">
+            <table class="w-full table-auto">
+                <thead>
+                    <tr class="bg-gray-200 text-gray-600 text-sm leading-normal sticky top-0">
+                        <th class="py-3 px-6 text-right whitespace-nowrap">کد حسابداری</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">شرکت</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">عنوان</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">نام</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">نام خانوادگی</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">نام مستعار</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">دسته‌بندی</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">نوع</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">اعتبار مالی (ریال)</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">تلفن ثابت</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">تلفن همراه</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">فکس</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">ایمیل</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">وب‌سایت</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">آدرس</th>
+                        <th class="py-3 px-6 text-right whitespace-nowrap">عملیات</th>
                     </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="text-gray-600 text-sm">
+                    <?php while ($row = $result->fetch_assoc()): ?>
+                        <tr class="border-b border-gray-200 hover:bg-gray-100">
+                            <td class="py-3 px-6 text-right">
+                                <a href="view_person.php?id=<?php echo $row['id']; ?>" class="text-blue-600 hover:text-blue-800">
+                                    <?php echo htmlspecialchars($row['code_hesabdari']); ?>
+                                </a>
+                            </td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['company']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['title']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['name']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['family']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['nickname']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['category']); ?></td>
+                            <td class="py-3 px-6 text-right">
+                                <div class="flex flex-col space-y-1">
+                                    <?php
+                                    if ($row['type_customer']) echo '<span class="inline-block bg-blue-100 text-blue-800 px-2 py-1 rounded text-xs">مشتری</span>';
+                                    if ($row['type_supplier']) echo '<span class="inline-block bg-green-100 text-green-800 px-2 py-1 rounded text-xs">تامین کننده</span>';
+                                    if ($row['type_shareholder']) echo '<span class="inline-block bg-yellow-100 text-yellow-800 px-2 py-1 rounded text-xs">سهامدار</span>';
+                                    if ($row['type_employee']) echo '<span class="inline-block bg-purple-100 text-purple-800 px-2 py-1 rounded text-xs">کارمند</span>';
+                                    ?>
+                                </div>
+                            </td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo number_format($row['credit']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['telephone']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['mobile']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['fax']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['email']); ?></td>
+                            <td class="py-3 px-6 text-right whitespace-nowrap"><?php echo htmlspecialchars($row['website']); ?></td>
+                            <td class="py-3 px-6 text-right max-w-xs overflow-hidden">
+                                <div class="truncate" title="<?php echo htmlspecialchars($row['address_text']); ?>">
+                                    <?php echo htmlspecialchars($row['address_text']); ?>
+                                </div>
+                            </td>
+                            <td class="py-3 px-6 text-right">
+                                <div class="flex item-center justify-end">
+                                    <a href="edit_person.php?id=<?php echo $row['id']; ?>" class="text-blue-600 hover:text-blue-900 ml-2" title="ویرایش">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="delete_person.php?id=<?php echo $row['id']; ?>" class="text-red-600 hover:text-red-900 ml-2" 
+                                       onclick="return confirm('آیا از حذف این فرد اطمینان دارید؟')" title="حذف">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
+
+<style>
+    .overflow-x-auto {
+        overflow-x: auto;
+        max-height: calc(100vh - 200px);
+    }
+    .min-w-max {
+        min-width: max-content;
+    }
+    thead tr {
+        position: sticky;
+        top: 0;
+        background: #f1f1f1;
+        z-index: 10;
+    }
+</style>
